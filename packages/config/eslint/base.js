3208e2d6d08e3b4c5175553da00db2edb3ff4e44
nextgenentrepreneurs91 <nextgenentrepreneurs91@gmail.com>
2026-04-26 15:53:51 +0530

    Update base.js

--- packages/config/eslint/base.js
+++ packages/config/eslint/base.js
@@ -1,1 +1,45 @@
+### packages/config/eslint/base.js
+```javascript
+/**
+ * ============================================================
+ * FILE: packages/config/eslint/base.js
+ * ============================================================
+ */
 
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'no-debugger': 'error',
+    'prefer-const': 'error'
+  }
+};
+```
